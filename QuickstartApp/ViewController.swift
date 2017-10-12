d348cd4a2eff67c5c2c79ee54502edda87eee56c
York Stanham <yorkstanham@Yorks-MacBook-Air.local>
2017-10-12 15:21:18 +1100

    Initial Commit

---
+++ QuickstartApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickstartApp
+//
+//  Created by York Stanham on 12/10/17.
+//  Copyright © 2017 York Stanham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
